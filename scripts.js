7c33e54c79ddc84ad79ac4f8f6f06f9971677d7f
Audra T <audraturuta@gmail.com>
2020-06-03 13:52:02 -0400

    error handling to ensure all questions have been answered

--- scripts.js
+++ scripts.js
@@ -1,34 +1,32 @@
 // ! Document Ready 
 
-    // Storing user inputs in variables
-    const userCaffeine = document.querySelector('input[name="caffeine"]:checked').value; 
+// Storing the form in a variable
+const form = document.querySelector('form');
 
-    const userFeeling = document.querySelector('input[name="feeling"]:checked').value;
+// Sumbitting the form
+form.addEventListener('submit', (e) =>{ 
+    // Prevent Default Submit
+    e.preventDefault();
     
-    const userSeason = document.querySelector('input[name="season"]:checked').value;
+    // Ensure a selection has been made for each question
+    const storeInputs = function () {
+        if (document.querySelector('input[name="caffeine"]:checked') === null || document.querySelector('input[name="feeling"]:checked') === null || document.querySelector('input[name="season"]:checked') === null) {
+            alert('Please pick');
+        }
+    }
+    storeInputs();
+    // const userCaffeine = document.querySelector('input[name="caffeine"]:checked').value; 
+    
+    // const userFeeling = document.querySelector('input[name="feeling"]:checked').value;
+    
+    // const userSeason = document.querySelector('input[name="season"]:checked').value;
 
-    // Storing the form in a variable
-    const form = document.querySelector('form');
-
-    // Functions
-    // const onClick = function (event) {
-    //     // Prevent Default
-    //     event.preventDefault();
-        
-    //     // Ensure user has selected a choice for each question
-    //     if (userCaffeine === null || userFeeling === null || userSeason === null) {
+    // // Ensure user has selected a choice for each question
+    // if (userCaffeine === null || userFeeling === null || userSeason === null) {
     //         alert('Please answer all of the questions')
     //     };
-    // };
-    
-    // Storing buttons/forms in variables
-    // const submitButton = document.getElementById('submit');
+});
 
-
-    form.addEventListener('submit', (e) =>{ 
-    e.preventDefault();
-    })
-
-    // // On click of the submit button
+// // On click of the submit button
     // submitButton.addEventListener('click', onClick);
 
