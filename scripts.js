5b9e3af854a175c9cb779c0882e9012c35cad9b7
Audra T <audraturuta@gmail.com>
2020-06-03 14:36:05 -0400

    storing user  inputs

--- scripts.js
+++ scripts.js
@@ -3,30 +3,35 @@
 // Storing the form in a variable
 const form = document.querySelector('form');
 
+// Functions
+const checkInputs = function () {
+    if (document.querySelector('input[name="caffeine"]:checked') === null || document.querySelector('input[name="feeling"]:checked') === null || document.querySelector('input[name="season"]:checked') === null) {
+        alert('Please pick');
+    }
+};
+
 // Sumbitting the form
 form.addEventListener('submit', (e) =>{ 
     // Prevent Default Submit
     e.preventDefault();
-    
     // Ensure a selection has been made for each question
-    const storeInputs = function () {
-        if (document.querySelector('input[name="caffeine"]:checked') === null || document.querySelector('input[name="feeling"]:checked') === null || document.querySelector('input[name="season"]:checked') === null) {
-            alert('Please pick');
-        }
-    }
-    storeInputs();
-    // const userCaffeine = document.querySelector('input[name="caffeine"]:checked').value; 
+    checkInputs();
+    // Store Inputs in variables
+    const userCaffeine = document.querySelector('input[name="caffeine"]:checked').value; 
     
-    // const userFeeling = document.querySelector('input[name="feeling"]:checked').value;
+    const userFeeling = document.querySelector('input[name="feeling"]:checked').value;
     
-    // const userSeason = document.querySelector('input[name="season"]:checked').value;
+    const userSeason = document.querySelector('input[name="season"]:checked').value;
+    console.log(userCaffeine, userFeeling, userSeason);
 
-    // // Ensure user has selected a choice for each question
-    // if (userCaffeine === null || userFeeling === null || userSeason === null) {
-    //         alert('Please answer all of the questions')
-    //     };
 });
 
-// // On click of the submit button
-    // submitButton.addEventListener('click', onClick);
+//! hide the tea types
+// user clicks submit
+// prevent default
+// check for answers
+// store inputs
+//! compare them to the tea types
+//! pick the tea
+//! display the correct tea type
 
